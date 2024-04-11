14afb6444d7b36ff73dd89ae86d4bf67a318873d
dk649 <58308212+dk649@users.noreply.github.com>
2024-04-11 17:45:53 +0100

    updates
    movie card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FiThumbsUp } from "react-icons/fi";
 
 const Card = ({ result }) => {
   return (
-    <div>
+    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2">
+      <h2 className="text-lg p-1 font-semibold truncate">
+        {result.title || result.name}
+      </h2>
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -12,8 +16,18 @@ const Card = ({ result }) => {
           }`}
           width={500}
           height={300}
-          className="sm:rounded-t-lg"
+          size="100vw"
+          alt="movie"
+          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
         ></Image>
+        <div className="p-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <p className="flex items-center gap-2">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp />
+            {result.vote_count}
+          </p>
+        </div>
       </Link>
     </div>
   );
